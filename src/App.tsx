c0476d1401da6ddee5f324a1a1f50e0deac38459
Ryley Hynes <ryleyhynes@gmail.com>
2025-11-02 10:14:01 -0500

    setting up bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0)
